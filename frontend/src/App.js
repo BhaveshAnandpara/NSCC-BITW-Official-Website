5a26964a742c0046c01b6644c45c4c05cd1f91d5
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-11-01 22:11:33 +0530

    Team Page Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import EventPage from './EventPage/EventPage';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar/Navbar';
 import Social from './Social/Social';
+import Team from './team/Team';
 
 import './App.css'
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <div className="bg-spin"></div>
 
         <Routes>
-          <Route path='/' element={<Social />} />
+          <Route path='/' element={<Team />} />
         </Routes>
         <Navbar/>
     </>
