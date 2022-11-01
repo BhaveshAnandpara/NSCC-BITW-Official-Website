29812273a9db8d84758cb2b06b1d8f65a9c54468
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-11-01 23:10:56 +0530

    Hero Section  Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,17 @@ import Navbar from './Navbar/Navbar';
 import Social from './Social/Social';
 import Team from './team/Team';
 import AboutUs from './AboutUs/AboutUs';
-
+import HeroSection from './HeroSection/HeroSection';
 import './App.css'
+
+
 function App() {
   return (
     <>
       <div className="bg-spin"></div>
 
         <Routes>
-          <Route path='/' element={<AboutUs />} />
+          <Route path='/' element={<HeroSection />} />
         </Routes>
         <Navbar/>
     </>
---
+++ frontend/src/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HeroSection.css'
+import logo from '../assets/Images/NSCC_Logo.svg'
+
+export default function HeroSection() {
+  return (
+    <>
+        <section className="hero-section ">
+            <img src={logo} alt="" />
+            <p className='overlay' >NSCC BITW is an outcome-oriented coding community that aims to upskill all its members and ultimately make them have successful careers.</p>
+        </section>
+    </>
+  )
+}
