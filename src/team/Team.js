589894af560feed03feb8af5e184ab7e87e68a40
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-11-22 19:49:57 +0530

    Frontend Was removed from Backend

---
+++ src/team/Team.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Team.css'
+import hands from '../assets/Icons/hands.svg'
+
+export default function Team() {
+  return (
+    <>
+    <section className="team-container">
+
+      <h1 className="titles" >
+        <center>
+          Meet The TEAM
+          <img className="m-4" src={hands} alt="" />
+        </center>
+      </h1>
+
+      <div className="team-content overlay  ">
+      We have an amazing team filled with creative and hard working minds
+      </div>
+
+      <button className='btn glass'  onClick={()=>{window.location.href = '/team'}} > <span className='overlay'> The Team </span></button>
+
+    </section>
+    </>
+
+  )
+}
+ 
